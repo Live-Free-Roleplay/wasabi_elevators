156a2f6757b2c0fcc444965bd3e2bcd38bdbab5b
Clink123 <clink123gamer@gmail.com>
2024-04-10 17:38:12 -0400

    Add custom changes

--- config.lua
+++ config.lua
@@ -11,8 +11,28 @@ Config.Elevators = {
         [1] = {
             coords = vec3(332.37, -595.56, 43.28), -- Coords, if you're new; last number is heading
             heading = 70.65, -- Heading of how will spawn on floor
-            title = 'Floor 2', -- Title 
+            title = 'Level 3', -- Title 
             description = 'Main Floor', -- Description
+            target = { -- Target length/width
+                width = 5,
+                length = 4
+            },
+        },
+        [2] = {
+            coords = vec3(344.31, -586.12, 28.79), -- Coords, if you're new; last number is heading
+            heading = 252.84,
+            title = 'Level 1',
+            description = 'Lower Floor',
+            target = {
+            width = 5,
+            length = 4
+            } -- Example without group
+        },
+        [3] = {
+            coords = vec3(338.5558, -583.7470, 74.1618), -- Coords, if you're new; last number is heading
+            heading = 82.5404, -- Heading of how will spawn on floor
+            title = 'Roof', -- Title 
+            description = 'Helipad', -- Description
             target = { -- Target length/width
                 width = 5,
                 length = 4
@@ -21,16 +41,6 @@ Config.Elevators = {
                 'police',
                 'ambulance'
             },
-        },
-        [2] = {
-            coords = vec3(344.31, -586.12, 28.79), -- Coords, if you're new; last number is heading
-            heading = 252.84,
-            title = 'Floor 1',
-            description = 'Lower Floor',
-            target = {
-            width = 5,
-            length = 4
-            } -- Example without group
         },
     },
 }
